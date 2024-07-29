536cae02aba0437011faa54335afe08aa27b85a0
Thiago Kadia Dias <thiagokadia@hotmail.com>
2024-07-29 15:19:58 -0300

    Delete unnecessary files/code from project

--- src/main/java/com/example/webgraph/SearchController.java
+++ src/main/java/com/example/webgraph/SearchController.java
@@ -3,8 +3,9 @@ package com.example.webgraph;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,20 +15,6 @@ import java.util.Map;
 
 @Controller
 public class SearchController {
-
-    @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("searchForm", new SearchForm());
-        return "index";
-    }
-
-    @PostMapping("/search")
-    public String search(@ModelAttribute SearchForm searchForm, Model model) {
-        String result = fetchWikipediaIntroduction(searchForm.getQuery());
-        model.addAttribute("result", result);
-        model.addAttribute("searchForm", searchForm);
-        return "index";
-    }
 
     @GetMapping("/search")
     @ResponseBody
